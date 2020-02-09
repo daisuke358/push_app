2baf029cfc72745689ed9744c370c006789958bc
daisuke358 <e.big358@gmail.com>
2020-02-09 16:24:55 +0900

    ユーザー編集、一覧、削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 User.create!(
   name: 'abcde',
   email: 'aaabbbccc@abc.com',
-  password: 'foobar'
+  password: 'foobar',
+  admin: true
   )
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
